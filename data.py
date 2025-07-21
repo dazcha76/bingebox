d77a1d2eda07f4a99069664c82652ad097d0bad4
Danika <dazcha@hotmail.com>
2025-07-21 12:36:01 +0100

    Added seeds for actors and episodes

---
+++ data.py
@@ -0,0 +1,25 @@
+real_tv_shows = [
+  "Breaking Bad",
+  "Stranger Things",
+  "Game of Thrones",
+  "The Office",
+  "Friends",
+  "The Crown",
+  "The Mandalorian",
+  "Sherlock",
+  "Westworld",
+  "The Simpsons"
+]
+
+real_actors = [
+  {"first_name": "Bryan", "last_name": "Cranston"},
+  {"first_name": "Millie Bobby", "last_name": "Brown"},
+  {"first_name": "Emilia", "last_name": "Clarke"},
+  {"first_name": "Steve", "last_name": "Carell"},
+  {"first_name": "Jennifer", "last_name": "Aniston"},
+  {"first_name": "Olivia", "last_name": "Colman"},
+  {"first_name": "Pedro", "last_name": "Pascal"},
+  {"first_name": "Benedict", "last_name": "Cumberbatch"},
+  {"first_name": "Evan Rachel", "last_name": "Wood"},
+  {"first_name": "Dan", "last_name": "Castellaneta"}
+]
--- seed.py
+++ seed.py
@@ -1,24 +1,14 @@
 from faker import Faker
 from database import SessionLocal
 from enums import ShowFormat, ShowGenre
+from data import real_actors, real_tv_shows
 import models
 import random
 
 fake = Faker()
 db = SessionLocal()
 
-real_tv_shows = [
-    "Breaking Bad",
-    "Stranger Things",
-    "Game of Thrones",
-    "The Office",
-    "Friends",
-    "The Crown",
-    "The Mandalorian",
-    "Sherlock",
-    "Westworld",
-    "The Simpsons"
-]
+# ------ Seed tv shows ------ #
 
 for name in real_tv_shows:
   show = models.Show(
@@ -29,6 +19,33 @@ for name in real_tv_shows:
     favorite=fake.boolean(),
   )
   db.add(show)
+
+# ------ Seed actors ------ #
+
+for actor_data in real_actors:
+  actor = models.Actor(
+    first_name=actor_data["first_name"],
+    last_name=actor_data["last_name"]
+  )
+  db.add(actor)
+
+# ------ Seed episodes ------ #
+
+shows = db.query(models.Show).all()
+
+for _ in range(len(shows)):
+  show = random.choice(shows)
+  episode_number = random.randint(1, 10)
+  title = fake.sentence(nb_words=5)
+  air_date = fake.date_this_decade()
+
+  episode = models.Episode(
+    number=episode_number,
+    title=title,
+    air_date=air_date,
+    show_id=show.id
+  )
+  db.add(episode)
 
 db.commit()
 db.close()
